372232c7867e8b507eb24b9463e9a131afd03d3b
khlupnov <m.khlupnov@gmail.com>
2020-04-30 17:20:11 +0300

    Initial upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+namespace IoTSimulation
+{
+    
+    class Program
+    {
+
+        private static ILogger logger;
+        public static IConfigurationRoot Configuration;
+        static void Main(string[] args)
+        {
+
+
+        using (var loggerFactory = LoggerFactory.Create(builder => {
+                    builder.AddFilter("Microsoft", LogLevel.Warning)
+                        .AddFilter("System", LogLevel.Warning)
+                        .AddFilter("IoTSimulation.Program", LogLevel.Debug)
+                        .AddConsole();
+                }
+            )){
+
+        logger = loggerFactory.CreateLogger<Program>();
+
+      
+       logger.LogInformation("Logging information.");
+            logger.LogCritical("Logging critical information.");
+            logger.LogDebug("Logging debug information.");
+            logger.LogError("Logging error information.");
+            logger.LogTrace("Logging trace");
+            logger.LogWarning("Logging warning.");
+   
+
+        };
+
+        }
+        
+    }
+}
+
+
